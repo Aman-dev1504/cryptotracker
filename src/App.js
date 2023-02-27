b1a9a3f1df157498938bd0c93c1812bbe0ed7d58
Aman <Amanmalviya786an@gmail.com>
2023-02-27 15:52:31 +0530

    d

--- src/App.js
+++ src/App.js
@@ -10,10 +10,9 @@ function App() {
     coin.name.toLowerCase().includes(search.toLowerCase())
   );
   useEffect(() => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     .then(res => {
       setCoins(res.data);
-      console.log(res.data);
     })
     .catch(error => console.log(error));
 }, []);
